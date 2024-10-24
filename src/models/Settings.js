05ee65408b049b7158e7d8093964e01f1a2670b9
qlzv <qatadaazzeh@repixel-agency.com>
2024-10-24 23:38:31 +0300

    Edit Settings Schema

--- src/models/Settings.js
+++ src/models/Settings.js
@@ -55,6 +55,59 @@ const SettingsSchema = new Schema({
       },
     ],
   },
+  Aboutus: {
+    MainText: {
+      type: String,
+      required: true,
+    },
+    Paragraph: {
+      type: String,
+      required: true,
+    },
+
+    Photo: {
+      Url: {
+        type: String,
+        required: true,
+      },
+    },
+    Features: {
+      First: {
+        Main: {
+          type: String,
+          required: true,
+        },
+        Paragraph: {
+          type: String,
+          required: true,
+        },
+      },
+      Second: {
+        Main: {
+          type: String,
+          required: true,
+        },
+        Paragraph: {
+          type: String,
+          required: true,
+        },
+      },
+      Third: {
+        Main: {
+          type: String,
+          required: true,
+        },
+        Paragraph: {
+          type: String,
+          required: true,
+        },
+      },
+    },
+    LastText: {
+      type: String,
+      required: true,
+    },
+  },
 });
 
 export default model("Settings", SettingsSchema);
