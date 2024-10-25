26681953328707d2e805dad2897bb1b76de7c13a
qlzv <qatadaazzeh@repixel-agency.com>
2024-10-25 19:44:37 +0300

    Added view product

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,9 +8,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import { router as aboutUsRouter } from "./aboutus.js";
 import { router as prodcutsRouter } from "./products.js";
+import { router as dashboardRouter } from "./viewproduct.js";
 router.use("/about-us", aboutUsRouter);
 router.use("/products", prodcutsRouter);
-// router.use("/dashboard", dashboardRouter);
+
+router.use("/view_product", dashboardRouter);
 
 router.get("/", (req, res) => {
   res.render(path.join(__dirname, "../../view/index"));
