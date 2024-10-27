ba90d372bfec6b169d9d29315da7e9c0a6110cdc
qlzv <qatadaazzeh@repixel-agency.com>
2024-10-27 22:21:04 +0300

    add signup fix login

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,11 +11,13 @@ import { router as prodcutsRouter } from "./products.js";
 import { router as dashboardRouter } from "./viewproduct.js";
 import { router as cartRouter } from "./cart.js";
 import { router as loginRouter } from "./login.js";
+import { router as signupRouter } from "./signup.js";
 router.use("/about-us", aboutUsRouter);
 router.use("/products", prodcutsRouter);
 router.use("/view_product", dashboardRouter);
 router.use("/cart", cartRouter);
 router.use("/login", loginRouter);
+router.use("/signup", signupRouter);
 router.get("/", (req, res) => {
   res.render(path.join(__dirname, "../../view/index"));
 });
